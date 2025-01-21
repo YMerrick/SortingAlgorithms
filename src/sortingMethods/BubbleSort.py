1f1205bb30934e7e4e299e37b55e84193fecd21b
York <sc17yxm@leeds.ac.uk>
2025-01-21 23:36:35 +0000

    implement bubble sort

--- src/sortingMethods/BubbleSort.py
+++ src/sortingMethods/BubbleSort.py
@@ -1,5 +1,13 @@
 from .SortInterface import SortInterface
 
 class BubbleSort(SortInterface):
-    def sort(nums):
-        return super().sort()
+    def sort(nums, reverse = None):
+        if not reverse:
+            compare_function = lambda a, b: a > b
+        else:
+            compare_function = lambda a, b: a < b
+        for n in range(len(nums), -1, -1):
+            for i in range(1, n):
+                if compare_function(nums[i - 1], nums[i]):
+                    nums[i - 1], nums[i] = nums[i], nums[i - 1]
+        return None
