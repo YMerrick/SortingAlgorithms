565849d8ea9b4e760d462de6425fb28ae190d6e7
York <sc17yxm@leeds.ac.uk>
2025-01-22 00:12:08 +0000

    implement selection sort

--- src/sortingMethods/SelectionSort.py
+++ src/sortingMethods/SelectionSort.py
@@ -3,4 +3,11 @@ from .SortInterface import SortInterface
 class SelectionSort(SortInterface):
     def sort(nums: list[int], reverse: bool = None):
         if not reverse:
-            pass
+            compare_function = lambda unsorted_subset: min(unsorted_subset)
+        else:
+            compare_function = lambda unsorted_subset: max(unsorted_subset)
+        for i in range(len(nums)):
+            unsorted_subset = nums[i:]
+            value = compare_function(unsorted_subset)
+            index_of_value = unsorted_subset.index(value) + i
+            nums[i], nums[index_of_value] = nums[index_of_value], nums[i]
